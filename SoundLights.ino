b3a27f7404bfa653ca94fcd0b5346ab5af7bfa40
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2019-01-17 22:11:53 +0000

    kinda working, a little jittery

--- SoundLights.ino
+++ SoundLights.ino
@@ -1,64 +1,92 @@
 #include <Adafruit_NeoPixel.h>
 
-
 const int LED_PIN = 6;
-const int NUMPIXELS = 12;
-const int DC_OFFSET = 0;
-const int NOISE = 100;
-const int SAMPLES = 60;
-const int TOP = NUMPIXELS + 1;
-const int DETECTOR_PIN = 0;
+const int NUM_PIXELS = 12;
+const int MIC_PIN = 0;
+const int DC_OFFSET = 330;
+const int NOISE = 10;
+const int GAIN = 10;
+const int SAMPLES = 5;
+const int MAX_BRIGHTNESS = 255;
 
-byte
-    peak = 0,
-    dotCount = 0,
-    volCount = 0;
-
+byte volCount = 0;
 int
     vol[SAMPLES],
     lvl = 10,
     minLvlAvg = 0,
-    maxLvlAvg = 512;
+    maxLvlAvg = 0;
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
-
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-    // initialise NeoPixel library
-    memset(vol, 0, sizeof(int)*SAMPLES);
+
+    for (int i = 0; i < SAMPLES; i++) {
+        vol[i] = 0;
+    }
     pixels.begin();
-    // Serial.begin(9600);
+
+    Serial.begin(9600);
 }
 
 void loop() {
-    
+    uint8_t i;
+    uint16_t minLvl, maxLvl;
+    int val, brightness;
 
+    val = analogRead(MIC_PIN);
+    val = abs(val - DC_OFFSET); // center on 0
+    val = (val <= NOISE) ? 0 : (val - NOISE);
+    lvl = ((lvl * 7) + val) >> 3; // dampen reading
 
-    // int val = analogRead(DETECTOR_PIN);
-    // if (abs(val - lastval) > 20) {
-    //     lastval = val;
-    //     // setBrightness(map(val, 0, 1023, 0, 255));
-    //     Serial.println(map(val, 0, 1023, 0, 255));
+    // brightness = MAX_BRIGHTNESS * (lvl - minLvlAvg) / (long)(maxLvlAvg - minLvlAvg);
+
+    brightness = map(lvl, 0, 1023, 0, 255);
+
+    // brightness = val;
+    // if (brightness > 255) {
+    //     brightness = 255;
     // }
-    // delay(5);
-}
 
-// void setBrightness(int b) {
-//     for (int p=0; p<NUMPIXELS; p++) {
-//         pixels.setPixelColor(p, pixels.Color(b, b, b));
-//     }
-//     pixels.show();
-// }
+    Serial.println(brightness);
+
+    if (brightness < 0L) {
+        brightness = 0;
+    } else if (brightness > MAX_BRIGHTNESS) {
+        brightness = MAX_BRIGHTNESS;
+    }
+
+    for (int i = 0; i < NUM_PIXELS; i++) {
+        pixels.setPixelColor(i, brightness, brightness, brightness);
+    }
+    pixels.show();
+
+    vol[volCount] = val; // save sample for dynamic leveling
+    if (++volCount >= SAMPLES) {
+        volCount = 0;
+    }
+
+    // maxLvl = vol[0];
+    // for (int i = 0; i < SAMPLES; i++) {
+    //     if (vol[i] > maxLvl) {
+    //         maxLvl = vol[i];
+    //     }
+    // }
 
 
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
+    // get volume range of previous frames
+    minLvl = maxLvl = vol[0];
+    for (i = 1; i < SAMPLES; i++) {
+        if (vol[i] < minLvl) {
+            minLvl = vol[i];
+        } else if (vol[i] > maxLvl) {
+            maxLvl = vol[i];
+        }
+    }
+
+    if ((maxLvl - minLvl) < MAX_BRIGHTNESS) {
+        maxLvl = minLvl + MAX_BRIGHTNESS;
+    }
+
+    minLvlAvg = (minLvlAvg * 63 + minLvl) >> 6;
+    maxLvlAvg = (maxLvlAvg * 63 + maxLvl) >> 6;
 }
