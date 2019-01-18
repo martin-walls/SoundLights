cf084bc59cec321619f910e7bee45dff6ea9646d
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2019-01-18 17:13:15 +0000

    rm samples array and dynamic level adjustment using average of samples

--- SoundLights.ino
+++ SoundLights.ino
@@ -6,15 +6,11 @@ const int MIC_PIN = 0;
 const int POT_PIN = 1;
 const int DC_OFFSET = 330;
 const int NOISE = 10;
-const int NUM_SAMPLES = 100;
 const int MAX_BRIGHTNESS = 255;
 const int MIN_TOP = 70;
 
-byte samplePos = 0;
 int
-    samples[NUM_SAMPLES],
     lvl = 10,
-    minLvlAvg = 0,
     localPeak = 0,
     localPeakTick = 0,
     maxLvl = 0;
@@ -22,10 +18,6 @@ int
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-
-    for (int i = 0; i < NUM_SAMPLES; i++) {
-        samples[i] = 0;
-    }
     pixels.begin();
 
     Serial.begin(9600);
@@ -41,14 +33,7 @@ void loop() {
 
     // brightness = MAX_BRIGHTNESS * (lvl - minLvlAvg) / (long)(maxLvlAvg - minLvlAvg);
 
-    if (lvl > localPeak) {
-        localPeak = ((localPeak * 3) + lvl) >> 2;
-        localPeakTick = millis();
-    } else if (millis() - localPeakTick > 5) {
-        samples[samplePos] = localPeak;
-        if (++samplePos > NUM_SAMPLES) {
-            samplePos = 0;
-        }
+    if (lvl > localPeak || millis() - localPeakTick > 5) {
         localPeak = ((localPeak * 3) + lvl) >> 2;
         localPeakTick = millis();
     }
@@ -75,59 +60,23 @@ void loop() {
     } else if (brightness > MAX_BRIGHTNESS) {
         brightness = MAX_BRIGHTNESS;
     }
-    Serial.println(maxLvl);
+
+    Serial.println(brightness);
 
     for (int i = 0; i < NUM_PIXELS; i++) {
         pixels.setPixelColor(i, brightness, brightness, brightness);
     }
     pixels.show();
+}
 
-
-    // maxLvl = samples[0];
-    // for (int i = 1; i < NUM_SAMPLES; i++) {
-    //     if (samples[i] > maxLvl) {
-    //         maxLvl = samples[i];
-    //     }
-    // }
-
-    // if (lvl > maxVal || millis() - maxValTick > 2000) {
-    //     maxVal = lvl;
-    //     maxValTick = millis();
-    // }
-    // if (maxLvl < 10) {
-    //     maxLvl = 10;
-    // }
-    // maxLvlAvg = (maxLvlAvg * 63 + maxLvl) >> 6;
-
-    // add small delay to help smooth output and make it less jittery
-    // delay(5);
-
-    // vol[volCount] = val; // save sample for dynamic leveling
-    // if (++volCount >= SAMPLES) {
-    //     volCount = 0;
-    // }
-    //
-    // // maxLvl = vol[0];
-    // // for (int i = 0; i < SAMPLES; i++) {
-    // //     if (vol[i] > maxLvl) {
-    // //         maxLvl = vol[i];
-    // //     }
-    // // }
-    //
-    //
-    // // get volume range of previous frames
-    // minLvl = maxLvl = vol[0];
-    // for (i = 1; i < SAMPLES; i++) {
-    //     if (vol[i] < minLvl) {
-    //         minLvl = vol[i];
-    //     } else if (vol[i] > maxLvl) {
-    //         maxLvl = vol[i];
-    //     }
-    // }
-    //
-    // if ((maxLvl - minLvl) < MAX_BRIGHTNESS) {
-    //     maxLvl = minLvl + MAX_BRIGHTNESS;
-    // }
-    //
-    // minLvlAvg = (minLvlAvg * 63 + minLvl) >> 6;
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
 }
